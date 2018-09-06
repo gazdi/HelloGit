06dcfe137756beaec48ac130aed39943d4175def
gazdi <43009709+gazdi@users.noreply.github.com>
2018-09-06 10:36:29 +0100

    Update TestHelloWorld.java

--- HelloSel/src/TestHelloWorld.java
+++ HelloSel/src/TestHelloWorld.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 //testcomment
+//test comment from a different source
 public class TestHelloWorld {
 
 	WebDriver browser;
